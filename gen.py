3cb2e36ed2bea83a0edff46dfaee86e22c369440
Ray Chen <tankimarshal2@gmail.com>
2021-12-11 15:36:33 +0000

    Fix gen.py

--- gen.py
+++ gen.py
@@ -47,6 +47,8 @@ with open("ignored.txt") as f:
     ignored = set(f.read().rstrip().splitlines())
 
 for name in os.listdir("archived"):
+    if not os.path.isdir(os.path.join("archived", "name")):
+        continue
     with open("archived/" + name + "/ignored.txt") as f:
         ignored.update(f.read().rstrip().splitlines())
     for file in glob.glob("archived/" + name + "/elements/**/*.txt", recursive=True):
